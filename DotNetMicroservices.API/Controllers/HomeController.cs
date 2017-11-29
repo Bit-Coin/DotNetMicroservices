862980e0841ad993c0f28f6a18c8a35b3a8ab4f1
Rodion Fedorov <rodionfedorov@outlook.com>
2017-11-29 16:56:44 +0200

    Move dbinitializer to separate docker-compose file to fix error in Visual Studio's docker extension

---
+++ DotNetMicroservices.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetMicroservices.API.Controllers
+{
+
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Api Endpoint";
+        }
+    }
+}
